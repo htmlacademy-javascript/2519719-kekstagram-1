9f41e43ad7b7dcc25552ae72b1caa0c71e422a0c
Iarina Barabash <yarinabarabash@MacBook-Pro-Arina.local>
2024-05-02 21:58:51 +0400

    fix

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-const MESSAGE = [
+const MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -23,10 +16,38 @@ const NAMES = [
   'Алина',
   'Володя'
 ];
+const DESCRIPTIONS = [
+  'Фоточка, а на ней красоточка',
+  'Это я на даче, сейчас дома уже',
+  'Подумать дважды? Прости, я не думаю даже единожды',
+  'Ну и кто кому теперь не пара, Володенька?'
+];
+
 const PHOTO_COUNT = 25;
+
+
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-function createRandomIdFromRangeGenerator (min, max) {
+const like = {
+  MIN: 15,
+  MAX: 200
+};
+
+const minAvatarInt = 1;
+const maxAvatarInt = 6;
+const minId = 1;
+const maxCommentId = 1000000000000;
+const minCommentValue = 1;
+const maxCommentValue = 10;
+
+
+const createRandomIdFromRangeGenerator = (min, max) => {
   const previousValues = [];
 
   return function () {
@@ -40,24 +61,31 @@ function createRandomIdFromRangeGenerator (min, max) {
     previousValues.push(currentValue);
     return currentValue;
   };
-}
-const createRandom1from25 = createRandomIdFromRangeGenerator(1, 25);
-const generateCommentId = createRandomIdFromRangeGenerator(1, 1000000000000);
-const createRandomPhotoId = createRandomIdFromRangeGenerator(1, 25);
+};
+const generateCommentId = createRandomIdFromRangeGenerator(minId, maxCommentId);
+const generatePhotoId = createRandomIdFromRangeGenerator(minId, PHOTO_COUNT);
 
-
-const createphoto = () => ({
-  id: createRandomPhotoId(),
-  url: `photos/${createRandom1from25()}.jpg`,
-  description: 'Фоточка, а на ней красоточка',
-  likes: getRandomInteger(15, 200),
-  comments : [{
-    id: generateCommentId(),
-    avatar: `avatar-${getRandomInteger(1, 6)}.svg`,
-    message: getRandomArrayElement(MESSAGE),
-    name: getRandomArrayElement(NAMES),
-  }],
+const createComment = () => ({
+  id: generateCommentId(),
+  avatar: `avatar-${getRandomInteger(minAvatarInt, maxAvatarInt)}.svg`,
+  message: getRandomArrayElement(MESSAGES),
+  name: getRandomArrayElement(NAMES),
 });
 
-Array.from({length: PHOTO_COUNT}, createphoto);
+
+const createPhoto = () => {
+  const id = generatePhotoId();
+  const comments = Array.from({length: getRandomInteger(minCommentValue, maxCommentValue)}, createComment);
+
+  return {
+    id: id,
+    url: `photos/${id}.jpg`,
+    description: getRandomArrayElement(DESCRIPTIONS),
+    likes: getRandomInteger(like.MIN, like.MAX),
+    comments : comments,
+  };
+};
+
+const generatePhotos = () => Array.from({length: PHOTO_COUNT}, createPhoto);
+generatePhotos();
 
