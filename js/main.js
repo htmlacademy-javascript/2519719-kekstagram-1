bd53d8d49b5444164fd2ee655216db8c3583d9cd
Iarina Barabash <yarinabarabash@MacBook-Pro-Arina.local>
2024-05-15 19:25:43 +0400

    add js/mocks.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-import './data.js';
-import './util.js';
-import './photo.js';
+import {generatePhotos} from './mocks.js';
+generatePhotos();
+
--- js/mocks.js
+++ js/mocks.js
@@ -1,13 +1,13 @@
-import {MESSAGES, NAMES, DESCRIPTIONS, PHOTO_COUNT, Like, MINAVATARINT, MAXAVATARINT, MINID,
-  MAXCOMMENTID, MINCOMMENTVALUE, MAXCOMMENTVALUE} from './data.js';
+import {MESSAGES, NAMES, DESCRIPTIONS, PHOTO_COUNT, Like, MIN_AVATAR, MAX_AVATAR, MIN_ID,
+  MAX_COMMENT_ID, MIN_COMMENT_VALUE, MAX_COMMENT_VALUE} from './data.js';
 import {createRandomIdFromRangeGenerator, getRandomArrayElement, getRandomInteger} from './util.js';
 
-const generateCommentId = createRandomIdFromRangeGenerator(MINID, MAXCOMMENTID);
-const generatePhotoId = createRandomIdFromRangeGenerator(MINID, PHOTO_COUNT);
+const generateCommentId = createRandomIdFromRangeGenerator(MIN_ID, MAX_COMMENT_ID);
+const generatePhotoId = createRandomIdFromRangeGenerator(MIN_ID, PHOTO_COUNT);
 
 const createComment = () => ({
   id: generateCommentId(),
-  avatar: `avatar-${getRandomInteger(MINAVATARINT, MAXAVATARINT)}.svg`,
+  avatar: `avatar-${getRandomInteger(MIN_AVATAR, MAX_AVATAR)}.svg`,
   message: getRandomArrayElement(MESSAGES),
   name: getRandomArrayElement(NAMES),
 });
@@ -15,7 +15,7 @@ const createComment = () => ({
 
 const createPhoto = () => {
   const id = generatePhotoId();
-  const comments = Array.from({length: getRandomInteger(MINCOMMENTVALUE, MAXCOMMENTVALUE)}, createComment);
+  const comments = Array.from({length: getRandomInteger(MIN_COMMENT_VALUE, MAX_COMMENT_VALUE)}, createComment);
 
   return {
     id: id,
@@ -27,4 +27,5 @@ const createPhoto = () => {
 };
 
 const generatePhotos = () => Array.from({length: PHOTO_COUNT}, createPhoto);
-generatePhotos();
+
+export {generatePhotos};
