35dd880aedec1c21b923cff806cfced9066f6991
Iarina Barabash <yarinabarabash@MacBook-Pro-Arina.local>
2024-06-26 17:04:59 +0400

    fix

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import { generatePhotos } from './mocks.js';
 import { renderMiniature } from './miniature.js';
-import { addPicturesEventListener } from './fullsize.js';
+import { addPicturesEventListener } from './fullsize-photo.js';
 
 const photos = generatePhotos();
 renderMiniature(photos);
